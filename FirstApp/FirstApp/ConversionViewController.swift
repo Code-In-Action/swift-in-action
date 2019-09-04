f69c4995a81fd54655c21438f470ad2cc4404b9f
Go7hic <gtfx0209@qq.com>
2019-09-04 23:57:49 +0800

    update

--- FirstApp/FirstApp/ConversionViewController.swift
+++ FirstApp/FirstApp/ConversionViewController.swift
@@ -8,5 +8,17 @@
 
 import UIKit
 class ConversionViewController: UIViewController {
-    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    @IBAction func fahrenHeitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty {
+                celsiusLabel.text = text
+        } else {
+              celsiusLabel.text = "???"
+        }
+        
+    }
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
 }
