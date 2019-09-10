3d6e922307ece8f3b6d43b2d15a3ef5a55f2920c
Go7hic <gtfx0209@qq.com>
2019-09-11 00:31:41 +0800

    update

---
+++ Buggy/Buggy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Buggy
+//
+//  Created by Go7hic on 2019/9/10.
+//  Copyright © 2019 Go7hic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        print("Called buttonTapped(:)");
+    }
+
+}
+
