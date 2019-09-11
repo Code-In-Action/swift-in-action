47b4ac05df6454cc8032a368837f630ff58902b7
Go7hic <gtfx0209@qq.com>
2019-09-12 00:03:35 +0800

    build: build test

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -14,8 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBAction func switchToggled(_ sender: UISwitch) {
+    @IBAction func buttonTapped(_ sender: UIButton) {
         print("Called buttonTapped(:)");
+     
+    }
+    func badMethod() {
+        let array = NSMutableArray();
+        for i in 0..<10{
+            array.insert(i, at: i)
+        }
+        
+        for _ in 0..<10 {
+            array.remove(0)
+        }
     }
 
 }
