c4a129b2d223c5e146a66afa05819d5a288a5333
Go7hic <gtfx0209@qq.com>
2019-10-17 23:45:58 +0800

    add project

---
+++ VacationSpots/VacationSpots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VacationSpots
+//
+//  Created by Go7hic on 2019/10/17.
+//  Copyright © 2019 Go7hic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
